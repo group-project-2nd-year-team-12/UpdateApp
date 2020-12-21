008201d6a4d5022df5c3d666e5c8138275f0e940
ishan <reshmikaediriweera1997@gmail.com>
2020-12-21 22:48:10 +0530

    Add login file using database

---
+++ app/src/main/java/com/example/demo/Background.java
@@ -0,0 +1,234 @@
+/*package com.example.demo;
+
+        import android.app.AlertDialog;
+        import android.content.Context;
+        import android.os.AsyncTask;
+
+        import java.io.BufferedReader;
+        import java.io.BufferedWriter;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.io.OutputStream;
+        import java.io.OutputStreamWriter;
+        import java.net.HttpURLConnection;
+        import java.net.MalformedURLException;
+        import java.net.URL;
+        import java.net.URLEncoder;*/
+
+/**
+ * Created by ProgrammingKnowledge on 1/5/2016.
+ */
+/*
+public class Background extends AsyncTask<String,Void,String> {
+    Context context;
+    AlertDialog alertDialog;
+    Background (Context ctx) {
+        context = ctx;
+    }
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String login_url = "http://192.168.1.11/login.php";
+        if(type.equals("login")) {
+            try {
+                String user_name = params[1];
+                String password = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}*/
+
+
+package com.example.demo;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.icu.util.Output;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class Background extends AsyncTask<String,Void,String> {
+
+    Context context;
+    Background(Context ctx){
+        context=ctx;
+    }
+    //ProgressDialog pdLoading ;
+   AlertDialog alertDialog;
+
+    @Override
+    protected String doInBackground(String... voids) {
+
+        String type=voids[0];
+        String login_url="http://10.0.2.2/Android/login.php";
+        if (type.equals("login")){
+            try {
+                String username=voids[1];
+                String password=voids[2];
+                URL url=new URL(login_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data= URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"+
+                        URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while ((line=bufferedReader.readLine())!=null){
+                    result+=line;
+
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog=new AlertDialog.Builder(context).create();
+    //    pdLoading= (ProgressDialog) new ProgressDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+//        alertDialog.setMessage(result);
+//        alertDialog.show();
+       // pdLoading.dismiss();
+        if (result.equals("LOGIN SUCCESS")){
+            Intent i = new Intent(context,Second.class);
+            context.startActivity(i);
+        }else{
+            alertDialog.setMessage(result);
+            alertDialog.show();
+        }
+
+//        if(result.equalsIgnoreCase("true"))
+//        {
+//
+////            Intent intent = new Intent(this,Second.class);
+////            startActivity(intent);
+////            MainActivity.this.finish();
+//
+//        }
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
+
+////MainActivity
+/*
+package com.example.demo;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText usernameEdit,passwordEdit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEdit=findViewById(R.id.username);
+        passwordEdit=findViewById(R.id.password);
+    }
+    public void onLogin(View view)
+    {
+        String username=usernameEdit.getText().toString();
+        String password=passwordEdit.getText().toString();
+        String type="login";
+
+        Background background=new Background(this);
+        background.execute(type,username,password);
+    }
+
+}*///
---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+EditText usernameEdit,passwordEdit;
+TextView tt ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEdit=(EditText) findViewById(R.id.username);
+        passwordEdit=(EditText) findViewById(R.id.password);
+
+    }
+    public void OnLogin(View view) throws ExecutionException, InterruptedException {
+        String username=usernameEdit.getText().toString();
+        String password=passwordEdit.getText().toString();
+       // tt =findViewById(R.id.textView);
+        String type="login";
+
+        Background background=new Background(this);
+       background.execute(type, username, password);
+//                if (result.equals("LOGIN SUCCESS")){
+//                    Intent i = new Intent(this,Second.class);
+//                    this.startActivity(i);
+//                  // tt.setText(result);
+//                }else{
+//                    AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+//                    alertDialog.setMessage(result);
+//                    alertDialog.show();
+//                }
+
+
+    }
+
+}
+
+

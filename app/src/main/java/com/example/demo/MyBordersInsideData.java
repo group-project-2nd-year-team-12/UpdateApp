200266ebd4236848a041f59962e4e4a2ced9926e
ishan <reshmikaediriweera1997@gmail.com>
2020-12-29 11:54:52 +0530

    Add  files

--- app/src/main/java/com/example/demo/MyBordersInsideData.java
+++ app/src/main/java/com/example/demo/MyBordersInsideData.java
@@ -4,23 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MyBordersInsideData extends AppCompatActivity {
 
-TextView textView;
+TextView txtemail,txttel,txtgender,txtinstitute;
+Button previous;
     String emailBoarder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_borders_inside_data);
-        textView=findViewById(R.id.email);
+
+        txtemail=findViewById(R.id.email);
+        txttel=findViewById(R.id.telephone);
+        txtgender=findViewById(R.id.gender);
+        txtinstitute=findViewById(R.id.institute);
+
+        previous=findViewById(R.id.btn_previous);
+
         Intent intent=getIntent();
-       // emailBoarder=intent.getStringExtra("email");
-        textView.setText(intent.getStringExtra("institute"));
-       // txtname.setText(intent.getStringExtra("first_name"));
+
+        txtinstitute.setText(intent.getStringExtra("institute"));
+        txtemail.setText(intent.getStringExtra("email"));
+        txttel.setText(intent.getStringExtra("telephone"));
+        txtgender.setText(intent.getStringExtra("gender"));
 
 
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(getApplicationContext(),MyBoarders.class);
+                startActivity(intent1);
+            }
+        });
 
 
     }

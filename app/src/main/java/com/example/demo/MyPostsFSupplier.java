d0f66e0987190adfe1c907dd706ad5cf4208d722
ishan <reshmikaediriweera1997@gmail.com>
2021-01-02 08:35:06 +0530

    add  food post page  in food supplier

---
+++ app/src/main/java/com/example/demo/MyPostsFSupplier.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPostsFSupplier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_posts_f_supplier);
+    }
+}
--- app/src/main/java/com/example/demo/ProfileEditB.java
+++ app/src/main/java/com/example/demo/ProfileEditB.java
@@ -1,26 +1,14 @@
 package com.example.demo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 public class ProfileEditB extends AppCompatActivity {
@@ -137,7 +125,7 @@ public class ProfileEditB extends AppCompatActivity {
                // txtTel.setText(telephone);
 
 
-        String type="updataB";
+    //    String type="updataB";
 
 
 //        BackgroundEditB background=new BackgroundEditB(this);

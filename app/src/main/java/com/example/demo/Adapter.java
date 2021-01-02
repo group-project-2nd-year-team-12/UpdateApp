2d4dfab1e00c97c5e4127bfcde4ba11742e13126
ishan <reshmikaediriweera1997@gmail.com>
2021-01-02 08:00:59 +0530

    Search practise codes

---
+++ app/src/main/java/com/example/demo/Adapter.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter  extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+    private List<Users> users;
+    private Context context;
+
+    public Adapter(List<Users> users, Context context) {
+        this.users = users;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.searchprac_list,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+      holder.name.setText(users.get(position).getName());
+      holder.name.setText(users.get(position).getEmail());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public  static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView name,email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name=itemView.findViewById(R.id.name);
+            email=itemView.findViewById(R.id.email);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/demo/Users.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Users  {
+
+    private int Id;
+    private String Name;
+    private String Email;
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+}

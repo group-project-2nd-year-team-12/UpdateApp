f0015218d15986408b49ab504c9fe438c1d1ce5d
ishan <reshmikaediriweera1997@gmail.com>
2021-01-04 01:50:28 +0530

    Updated Edit profile  Java files in Boarding Owner

---
+++ app/src/main/java/com/example/demo/ProfileEditBO.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.ExecutionException;
+
+public class ProfileEditBO extends AppCompatActivity {
+
+    EditText EditFirst,EditLast,Editnic,Editaddress;
+    String intentEmail;
+    Button Edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_edit_b_o);
+
+        EditFirst=findViewById(R.id.first_name);
+        EditLast=findViewById(R.id.last_name);
+        Editnic=findViewById(R.id.nic);
+        Editaddress=findViewById(R.id.address);
+
+        Intent intent=getIntent();
+        intentEmail=intent.getStringExtra("intentEmail");
+        EditFirst.setText(intent.getStringExtra("intentfirst_name"));
+        EditLast.setText(intent.getStringExtra("intentlast_name"));
+        Editnic.setText(intent.getStringExtra("intentnic"));
+        Editaddress.setText(intent.getStringExtra("intent_address"));
+
+        Edit=findViewById(R.id.update);
+
+
+
+
+
+
+    }
+
+    public void onUpdateBO(View view) throws ExecutionException, InterruptedException {
+        String first_name=EditFirst.getText().toString();
+        String last_name=EditLast.getText().toString();
+        String nic=Editnic.getText().toString();
+        String address=Editaddress.getText().toString();
+
+        //updateProfileBO.php
+        String type="updateProfileBO";
+
+        BackgroundEditBO backgroundEditBO=new BackgroundEditBO(this);
+        String result=backgroundEditBO.execute(type,intentEmail,first_name,last_name,nic,address).get();
+
+        if (result.equals("Successfully")){
+            Intent intent=new Intent(getApplicationContext(),ProfilePageBO.class);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(),"Successfully Updated",Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+}

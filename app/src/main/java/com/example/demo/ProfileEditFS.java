d2a63e1d34d29730ca72dacf0db3c89fe1ad5beb
ishan <reshmikaediriweera1997@gmail.com>
2021-01-04 23:59:47 +0530

    Updated Edit profile files in Food Supplier

--- app/src/main/java/com/example/demo/ProfileEditFS.java
+++ app/src/main/java/com/example/demo/ProfileEditFS.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.ExecutionException;
 
 public class ProfileEditFS extends AppCompatActivity {
     EditText EditFirst,EditLast,EditAddress,EditNic;
@@ -30,7 +33,7 @@ public class ProfileEditFS extends AppCompatActivity {
 
     }
 
-    public void onUpdateFS(View view) {
+    public void onUpdateFS(View view) throws ExecutionException, InterruptedException {
 
         String first_name=EditFirst.getText().toString();
         String last_name=EditLast.getText().toString();
@@ -38,6 +41,16 @@ public class ProfileEditFS extends AppCompatActivity {
         String nic=EditNic.getText().toString();
 
 
+        String type="updateProfileFS";
+
+        BackgroundEditFS backgroundEditFS=new BackgroundEditFS(this);
+        String result=backgroundEditFS.execute(type,emailShared,first_name,last_name,address,nic).get();
+
+        if (result.equals("Successfully")){
+            Intent intent=new Intent(getApplicationContext(),ProfilePageFS.class);
+            startActivity(intent);
+            Toast.makeText(getApplicationContext(),"Successfully Updated",Toast.LENGTH_LONG).show();
+        }
 
 
     }

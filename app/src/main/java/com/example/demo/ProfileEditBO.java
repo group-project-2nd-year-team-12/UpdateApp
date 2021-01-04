879837246a70792a73eec76cc51770c64029784f
ishan <reshmikaediriweera1997@gmail.com>
2021-01-04 10:20:06 +0530

    Updated Edit profile  Java files in Boarding Owner

--- app/src/main/java/com/example/demo/ProfileEditBO.java
+++ app/src/main/java/com/example/demo/ProfileEditBO.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,7 @@ public class ProfileEditBO extends AppCompatActivity {
     EditText EditFirst,EditLast,Editnic,Editaddress;
     String intentEmail;
     Button Edit;
+    TextView txtfirst,txtlast,txtnic,txtaddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,11 @@ public class ProfileEditBO extends AppCompatActivity {
         EditLast=findViewById(R.id.last_name);
         Editnic=findViewById(R.id.nic);
         Editaddress=findViewById(R.id.address);
+
+        txtfirst=findViewById(R.id.email1);
+        txtlast=findViewById(R.id.name1);
+        txtnic=findViewById(R.id.institute1);
+        txtaddress=findViewById(R.id.telephone1);
 
         Intent intent=getIntent();
         intentEmail=intent.getStringExtra("intentEmail");
@@ -41,6 +48,8 @@ public class ProfileEditBO extends AppCompatActivity {
 
 
 
+
+
     }
 
     public void onUpdateBO(View view) throws ExecutionException, InterruptedException {
@@ -49,6 +58,10 @@ public class ProfileEditBO extends AppCompatActivity {
         String nic=Editnic.getText().toString();
         String address=Editaddress.getText().toString();
 
+        txtfirst.setText(first_name);
+        txtlast.setText(last_name);
+        txtnic.setText(nic);
+        txtaddress.setText(address);
         //updateProfileBO.php
         String type="updateProfileBO";
 

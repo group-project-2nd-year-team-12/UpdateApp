9ae6c13069de2c959bfdb7f31d12f4e724b6d72c
ishan <reshmikaediriweera1997@gmail.com>
2021-01-06 21:16:45 +0530

    Add Add as new boarder files in Boarding owner

---
+++ app/src/main/java/com/example/demo/AddasBoarderBO.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddasBoarderBO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addas_boarder_b_o);
+    }
+}

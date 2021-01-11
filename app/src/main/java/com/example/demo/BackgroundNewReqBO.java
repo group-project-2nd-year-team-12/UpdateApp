4ff0a844828899ca9df5f6ee104db7b478038f79
ishan <reshmikaediriweera1997@gmail.com>
2021-01-11 10:34:06 +0530

    Added cancel request for database

--- app/src/main/java/com/example/demo/BackgroundNewReqBO.java
+++ app/src/main/java/com/example/demo/BackgroundNewReqBO.java
@@ -29,6 +29,7 @@ public class BackgroundNewReqBO extends AsyncTask<String,Void,String> {
 
         String type=strings[0];
         String update_url="http://10.0.2.2/Android/files/AcceptReqBO.php";
+        String cancel_url="http://10.0.2.2/Android/files/CancelReqBO.php";
         if (type.equals("AcceptReqBO")){
 
 
@@ -72,6 +73,51 @@ public class BackgroundNewReqBO extends AsyncTask<String,Void,String> {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }else if (type.equals("CancelReqBO")){
+
+            String request_id=strings[1];
+
+            try {
+                URL url=new URL(cancel_url);
+                HttpURLConnection httpURLConnection= (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data=URLEncoder.encode("request_id","UTF-8")+"="+URLEncoder.encode(request_id,"UTF-8");
+
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while ((line=bufferedReader.readLine())!=null){
+                    result+=line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+
+
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
         return null;
     }

fb355486b6ee99c28afc6fae41b562c3f3ed99c3
ishan <reshmikaediriweera1997@gmail.com>
2021-01-16 20:52:50 +0530

    updated edit profile page in bording owner

--- app/src/main/java/com/example/demo/ProfileEditBO.java
+++ app/src/main/java/com/example/demo/ProfileEditBO.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.concurrent.ExecutionException;
 
@@ -17,22 +17,25 @@ public class ProfileEditBO extends AppCompatActivity {
     EditText EditFirst,EditLast,Editnic,Editaddress;
     String intentEmail;
     Button Edit;
-    TextView txtfirst,txtlast,txtnic,txtaddress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_edit_b_o);
 
+
+        //set toolbar
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         EditFirst=findViewById(R.id.first_name);
         EditLast=findViewById(R.id.last_name);
         Editnic=findViewById(R.id.nic);
         Editaddress=findViewById(R.id.address);
 
-        txtfirst=findViewById(R.id.email1);
-        txtlast=findViewById(R.id.name1);
-        txtnic=findViewById(R.id.institute1);
-        txtaddress=findViewById(R.id.telephone1);
+
 
         Intent intent=getIntent();
         intentEmail=intent.getStringExtra("intentEmail");
@@ -44,12 +47,6 @@ public class ProfileEditBO extends AppCompatActivity {
         Edit=findViewById(R.id.update);
 
 
-
-
-
-
-
-
     }
 
     public void onUpdateBO(View view) throws ExecutionException, InterruptedException {
@@ -58,10 +55,7 @@ public class ProfileEditBO extends AppCompatActivity {
         String nic=Editnic.getText().toString();
         String address=Editaddress.getText().toString();
 
-        txtfirst.setText(first_name);
-        txtlast.setText(last_name);
-        txtnic.setText(nic);
-        txtaddress.setText(address);
+
         //updateProfileBO.php
         String type="updateProfileBO";
 

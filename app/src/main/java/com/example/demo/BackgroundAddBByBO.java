bada6fd64e4f1e0e24fe663f5f3c21021c356496
ishan <reshmikaediriweera1997@gmail.com>
2021-01-17 02:20:24 +0530

    updated AddasBoarderNew  database files

---
+++ app/src/main/java/com/example/demo/BackgroundAddBByBO.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class BackgroundAddBByBO extends AsyncTask<String,Void,String > {
+    Context context;
+    BackgroundAddBByBO(Context ctx){context=ctx;}
+    AlertDialog alertDialog;
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String type=strings[0];
+        String update_url="http://10.0.2.2/Android/files/UpdateAddasB.php";
+        if (type.equals("UpdateAddasB")){
+            String request_id=strings[1];
+            try {
+                URL url=new URL(update_url);
+                HttpURLConnection httpURLConnection= (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+
+                OutputStream outputStream=httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data= URLEncoder.encode("request_id","UTF-8")+"="+URLEncoder.encode(request_id,"UTF-8");
+
+
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+
+                String result="";
+                String line="";
+                while ((line=bufferedReader.readLine())!=null){
+                    result+=line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+}

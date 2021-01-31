f47b574ebb0d6c07d1fe0b983dbbbd0b662f178e
ishan <reshmikaediriweera1997@gmail.com>
2021-01-31 20:24:41 +0530

    Updated setting page in food supplier

--- app/src/main/java/com/example/demo/SettingFS.java
+++ app/src/main/java/com/example/demo/SettingFS.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SettingFS extends AppCompatActivity {
 
@@ -16,5 +21,29 @@ public class SettingFS extends AppCompatActivity {
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        //set home selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        //Perform ItemSelectedListener
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.dashbaord:
+                        startActivity(new Intent(getApplicationContext(),LongTermOrder.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
